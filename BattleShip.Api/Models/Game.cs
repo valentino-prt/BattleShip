08da5f19a9f45d0c2d598a44ec0ae596913883a1
Samuel Ohayon <97451292+samuel13584@users.noreply.github.com>
2024-02-06 15:58:56 +0100

    add id game model

--- BattleShip.Api/Models/Game.cs
+++ BattleShip.Api/Models/Game.cs
@@ -3,6 +3,7 @@
 public class Game
 {
     public Board Board { get; set; } // the board
+    public Guid Id {get;} = new Guid();
     public string Status { get; set; } // status of the game
     public string Winner { get; set; } // winner of the game
     public string Player { get; set; } // player whose turn it is
