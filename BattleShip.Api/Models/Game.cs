c4ca3572ec0adef7a82ff632382efd0002acc585
Valentin Poirot <valentin.poirot@outlook.com>
2024-02-06 16:18:36 +0100

    commit for game

--- BattleShip.Api/Models/Game.cs
+++ BattleShip.Api/Models/Game.cs
@@ -2,10 +2,7 @@
 
 public class Game
 {
-    public Board BoardUser { get; set; } // board for user
-    public Board BoardIa {get; set;} // board for IA
-    public Guid Id {get;} = new Guid();
-    public string Status { get; set; } // status of the game
-    public string Winner { get; set; } // winner of the game
-    public string Player { get; set; } // player whose turn it is
+    public List<Board> Boards { get; set; } = [new Board(), new Board()]; // boards for each player (0 for player 1, 1 for player Ai)
+    public Guid Id { get; } = new Guid();
+    public int Player { get; set; } = 0; // current player (0 or 1)
 }
