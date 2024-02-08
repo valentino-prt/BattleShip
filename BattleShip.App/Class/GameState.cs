1a33077653cece934331105d5f506700e9a5dd18
Samuel Ohayon <97451292+samuel13584@users.noreply.github.com>
2024-02-08 11:04:44 +0100

    connect api attack

--- BattleShip.App/Class/GameState.cs
+++ BattleShip.App/Class/GameState.cs
@@ -1,6 +1,7 @@
 public class GameState
 {
     private static GameState? instance;
+    public Guid GameId { get; set; }
 
     public char[,] UserBoard { get; set; }
     public char[,] AIBoard { get; set; }
--- BattleShip.Models/GameInitInfo.cs
+++ BattleShip.Models/GameInitInfo.cs
@@ -4,10 +4,4 @@ public class GameInitInfo
 {
     public Guid GameId { get; set; }
     public List<Ship> Ships { get; set; }
-
-    public GameInitInfo(Guid gameId, List<Ship> ships)
-    {
-        GameId = gameId;
-        Ships = ships;
-    }
 }
