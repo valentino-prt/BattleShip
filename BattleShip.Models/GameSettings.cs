2f5082591a3a594caa70e491dd7254064433d68a
Samuel Ohayon <97451292+samuel13584@users.noreply.github.com>
2024-02-28 11:59:15 +0100

    implement post new game and modify class structure for json

--- BattleShip.Models/GameSettings.cs
+++ BattleShip.Models/GameSettings.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace BattleShip.Models;
 
 public enum GameMode
@@ -15,32 +17,11 @@ public enum AIDifficulty
 
 public class GameSettings
 {
-    // Constructeur pour le mode SoloVsAI
-    public GameSettings(GameMode mode, AIDifficulty difficulty)
+    [JsonConstructor]
+    public GameSettings(GameMode mode, AIDifficulty? difficulty = null)
     {
-        if (mode == GameMode.SoloVsAI)
-        {
-            Mode = mode;
-            Difficulty = difficulty;
-        }
-        else
-        {
-            throw new ArgumentException("Difficulty should not be set for multiplayer mode.");
-        }
-    }
-
-    // Constructeur pour le mode Multiplayer
-    public GameSettings(GameMode mode)
-    {
-        if (mode == GameMode.Multiplayer)
-        {
-            Mode = mode;
-            Difficulty = null;
-        }
-        else
-        {
-            throw new ArgumentException("This constructor should only be used for multiplayer mode.");
-        }
+        Mode = mode;
+        Difficulty = difficulty;
     }
 
     public GameMode Mode { get; set; }
--- BattleShip.Models/Requests.cs
+++ BattleShip.Models/Requests.cs
@@ -1,21 +1,21 @@
 namespace BattleShip.Models;
 
-public class InitializeGameRequest
+public class InitializeGameRequest(Guid creatorId, GameSettings gameSettings)
 {
-    public Guid CreatorId { get; set; }
-    public GameSettings GameSettings { get; set; }
+    public Guid CreatorId { get; set; } = creatorId;
+    public GameSettings GameSettings { get; set; } = gameSettings;
 }
 
-public class AttackRequest
+public class AttackRequest(Guid gameId, Guid playerId, int x, int y)
 {
-    public Guid GameId { get; set; }
-    public Guid PlayerId { get; set; }
-    public int X { get; set; }
-    public int Y { get; set; }
+    public Guid GameId { get; set; } = gameId;
+    public Guid PlayerId { get; set; } = playerId;
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
 }
 
-public class TryJoinGameRequest
+public class TryJoinGameRequest(Guid sessionId, Guid playerId)
 {
-    public Guid SessionId { get; set; }
-    public Guid PlayerId { get; set; }
+    public Guid SessionId { get; set; } = sessionId;
+    public Guid PlayerId { get; set; } = playerId;
 }
