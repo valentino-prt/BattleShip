e0e9659d076119a63770477d6ff6276a5fba0956
Valentin Poirot <valentin.poirot@outlook.com>
2024-02-29 11:26:34 +0100

    done

--- BattleShip.App/Program.cs
+++ BattleShip.App/Program.cs
@@ -1,12 +1,14 @@
 using BattleShip.App;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//TODO changer la définition du client http où on défini l'adresse de base en tant que celle de l'api
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+const string ApiUrl = "https://localhost:5073";
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(ApiUrl) });
+
 GameState.InitializeInstance(Guid.NewGuid(), new char[10, 10], new char[10, 10], Guid.NewGuid());
 builder.Services.AddSingleton(GameState.Instance);
 
